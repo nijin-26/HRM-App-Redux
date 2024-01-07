8e3248cc29a9544061d19c500c831b21e43d2f8b
nijin-26 <nijin@qburst.com>
2024-01-07 13:13:12 +0530

    Fix: Deployement issue

--- src/components/EmployeesTableFilter/EmployeesTableFilter.style.ts
+++ src/components/EmployeesTableFilter/EmployeesTableFilter.style.ts
@@ -10,7 +10,71 @@ export const StyledEmployeesFilterWrap = styled.div`
 
 export const CustomSelectStyles: StylesConfig<
     IReactSelectOption,
-    boolean,
+    true,
+    GroupBase<IReactSelectOption>
+> = {
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: "#fff",
+        boxShadow: "none",
+        cursor: "text",
+        width: "100%",
+        minWidth: "200px",
+        height: "100%",
+        maxHeight: "100px",
+        overflow: "auto",
+        borderRadius: "3px",
+        borderColor: state.isFocused ? "#000" : "#7e7e7e",
+        "&:hover": {
+            borderColor: state.isFocused ? "#000" : "#7e7e7e",
+        },
+        outline: state.isFocused ? "1px solid #000" : "none",
+    }),
+    valueContainer: (provided) => ({
+        ...provided,
+        paddingTop: 0,
+        paddingBottom: 0,
+    }),
+    input: (provided) => ({
+        ...provided,
+        "& input": {
+            height: "auto",
+        },
+    }),
+    indicatorSeparator: (provided) => ({
+        ...provided,
+        marginTop: 0,
+        marginBottom: 0,
+        alignSelf: "center",
+        height: "60%",
+    }),
+    clearIndicator: (provided) => ({
+        ...provided,
+        cursor: "pointer",
+        color: "#7e7e7e",
+        paddingTop: 0,
+        paddingBottom: 0,
+    }),
+    dropdownIndicator: (provided) => ({
+        ...provided,
+        cursor: "pointer",
+        color: "#7e7e7e",
+        paddingTop: 0,
+        paddingBottom: 0,
+    }),
+    multiValueRemove: (provided) => ({
+        ...provided,
+        cursor: "pointer",
+    }),
+    singleValue: (provided) => ({
+        ...provided,
+        color: "black",
+    }),
+};
+
+export const CustomSingleSelectStyle: StylesConfig<
+    IReactSelectOption,
+    false,
     GroupBase<IReactSelectOption>
 > = {
     control: (provided, state) => ({
--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,5 +1,5 @@
 import { useSearchParams } from "react-router-dom";
-import { CustomSelectStyles } from "../EmployeesTableFilter/EmployeesTableFilter.style";
+import { CustomSingleSelectStyle } from "../EmployeesTableFilter/EmployeesTableFilter.style";
 import { SortContainer } from "./Sort.styles";
 import Select, { SingleValue } from "react-select";
 import { useState } from "react";
@@ -8,63 +8,63 @@ import { useAppDispatch } from "../../hooks/storeHelpers";
 import { employeeListClear } from "../../core/store/employeesList/actions";
 
 const Sort = () => {
-  const dispatch = useAppDispatch();
-  const [sort, setSort] = useState<SingleValue<IReactSelectOption>>({
-    label: "ID",
-    value: "id",
-  });
-  const [searchParams, setSearchParams] = useSearchParams();
+    const dispatch = useAppDispatch();
+    const [sort, setSort] = useState<SingleValue<IReactSelectOption>>({
+        label: "ID",
+        value: "id",
+    });
+    const [searchParams, setSearchParams] = useSearchParams();
 
-  const sortOptions: IReactSelectOption[] = [
-    { value: "id", label: "ID" },
-    { value: "firstName", label: "Name" },
-    { value: "email", label: "Email" },
-  ];
+    const sortOptions: IReactSelectOption[] = [
+        { value: "id", label: "ID" },
+        { value: "firstName", label: "Name" },
+        { value: "email", label: "Email" },
+    ];
 
-  const handleSortChange = (option: SingleValue<IReactSelectOption>) => {
-    dispatch(employeeListClear());
-    setSort(option);
-    searchParams.set("offset", "0");
-    searchParams.set("sortBy", option?.value!);
-    searchParams.set("sortDir", "asc");
-    setSearchParams(searchParams);
-  };
+    const handleSortChange = (option: SingleValue<IReactSelectOption>) => {
+        dispatch(employeeListClear());
+        setSort(option);
+        searchParams.set("offset", "0");
+        searchParams.set("sortBy", option?.value!);
+        searchParams.set("sortDir", "asc");
+        setSearchParams(searchParams);
+    };
 
-  const getCurrentSortDirection = () => {
-    const sortDirection = searchParams.get("sortDir");
+    const getCurrentSortDirection = () => {
+        const sortDirection = searchParams.get("sortDir");
 
-    if (sortDirection === "asc") return "arrow_upward";
-    else return "arrow_downward";
-  };
+        if (sortDirection === "asc") return "arrow_upward";
+        else return "arrow_downward";
+    };
 
-  const handleSortDirectionChange = () => {
-    dispatch(employeeListClear());
-    const sortDirection = searchParams.get("sortDir");
-    if (sortDirection === "asc") searchParams.set("sortDir", "desc");
-    else searchParams.set("sortDir", "asc");
-    setSearchParams(searchParams);
-  };
+    const handleSortDirectionChange = () => {
+        dispatch(employeeListClear());
+        const sortDirection = searchParams.get("sortDir");
+        if (sortDirection === "asc") searchParams.set("sortDir", "desc");
+        else searchParams.set("sortDir", "asc");
+        setSearchParams(searchParams);
+    };
 
-  return (
-    <SortContainer>
-      <Select
-        styles={CustomSelectStyles}
-        options={sortOptions}
-        placeholder={"Sort By"}
-        value={sort}
-        isMulti={false}
-        onChange={handleSortChange}
-      />
-      <div className="sort-icon">
-        <span
-          className="material-symbols-rounded"
-          onClick={handleSortDirectionChange}
-        >
-          {getCurrentSortDirection()}
-        </span>
-      </div>
-    </SortContainer>
-  );
+    return (
+        <SortContainer>
+            <Select
+                styles={CustomSingleSelectStyle}
+                options={sortOptions}
+                placeholder={"Sort By"}
+                value={sort}
+                isMulti={false}
+                onChange={handleSortChange}
+            />
+            <div className="sort-icon">
+                <span
+                    className="material-symbols-rounded"
+                    onClick={handleSortDirectionChange}
+                >
+                    {getCurrentSortDirection()}
+                </span>
+            </div>
+        </SortContainer>
+    );
 };
 
 export default Sort;
