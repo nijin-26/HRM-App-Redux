d90cc8ac5789d8b5c1823b436fed23b4fff85b2f
Nijin Nazar <nijin@qburst.com>
2024-01-08 13:08:31 +0530

    Fix: Renew token issue

--- src/core/api/services/auth.ts
+++ src/core/api/services/auth.ts
@@ -1,15 +1,15 @@
-import { API } from '..';
+import { API } from "..";
 import {
-    IAPISignInRequest,
-    IAPISignInResponse,
-} from '../../../interfaces/ApiDataInterface';
+  IAPISignInRequest,
+  IAPISignInResponse,
+} from "../../../interfaces/ApiDataInterface";
 
 export const signIn = (payload: IAPISignInRequest) => {
-    return API.post<IAPISignInResponse>('auth/sign-in', payload);
+  return API.post<IAPISignInResponse>("auth/sign-in", payload);
 };
 
 export const renewAccessToken = (currentRefreshToken: string) => {
-    return API.post<IAPISignInResponse>('auth/renew-token', {
-        currentRefreshToken,
-    });
+  return API.post<IAPISignInResponse>("auth/renew-token", {
+    refreshToken: currentRefreshToken,
+  });
 };
--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,56 +1,51 @@
-import { jwtDecode } from 'jwt-decode';
-import { renewAccessToken } from '../core/api/services/auth';
-import { toast } from 'react-toastify';
+import { jwtDecode } from "jwt-decode";
+import { renewAccessToken } from "../core/api/services/auth";
+import { toast } from "react-toastify";
 
-type cookieName = 'accessToken' | 'refreshToken';
+type cookieName = "accessToken" | "refreshToken";
 
 export const getCookie = (name: cookieName) => {
-    const value = `; ${document.cookie}`;
-    const parts: string[] = value?.split(`; ${name}=`);
-    if (parts.length === 2) {
-        return parts?.pop()?.split(';')?.shift();
-    }
-    return null;
+  const value = `; ${document.cookie}`;
+  const parts: string[] = value?.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts?.pop()?.split(";")?.shift();
+  }
+  return null;
 };
 
 export const removeCookie = (name: cookieName) => {
-    const expiredDate = new Date(0).toUTCString();
-    document.cookie = `${name}=; expires=${expiredDate}; path=/`;
+  const expiredDate = new Date(0).toUTCString();
+  document.cookie = `${name}=; expires=${expiredDate}; path=/`;
 };
 
 export const setCookie = (name: cookieName, value: string) => {
-    const decodedToken = jwtDecode(value);
-    const expiration = new Date(0);
+  const decodedToken = jwtDecode(value);
+  const expiration = new Date(0);
 
-    if (decodedToken.exp) {
-        expiration.setUTCSeconds(decodedToken.exp);
-    }
-    const cookieValue =
-        encodeURIComponent(value) +
-        (decodedToken.exp ? `; expires=${expiration.toUTCString()}` : '');
+  if (decodedToken.exp) {
+    expiration.setUTCSeconds(decodedToken.exp);
+  }
+  const cookieValue =
+    encodeURIComponent(value) +
+    (decodedToken.exp ? `; expires=${expiration.toUTCString()}` : "");
 
-    document.cookie = `${name}=${cookieValue}; path=/`;
+  document.cookie = `${name}=${cookieValue}; path=/`;
 };
 
 export const refreshTokens = async () => {
-    const currentRefreshToken = getCookie('refreshToken');
-    if (currentRefreshToken) {
-        try {
-            const { data: newTokens } = await renewAccessToken(
-                currentRefreshToken
-            );
-            setCookie('accessToken', newTokens.access_token);
-            setCookie('refreshToken', newTokens.refresh_token);
-            return newTokens;
-        } catch (error) {
-            toast.error(
-                'Unable to refresh your session. Please log in again to continue.'
-            );
-            console.log(error);
-            removeCookie('accessToken');
-            removeCookie('refreshToken');
-        }
-    } else {
-        return null;
+  const currentRefreshToken = getCookie("refreshToken");
+  if (currentRefreshToken) {
+    try {
+      const { data: newTokens } = await renewAccessToken(currentRefreshToken);
+      return newTokens;
+    } catch (error) {
+      toast.error(
+        "Unable to refresh your session. Please log in again to continue."
+      );
+      console.log("Refresh Token Error", error);
+      removeCookie("accessToken");
+      removeCookie("refreshToken");
+      return;
     }
+  } else return;
 };
